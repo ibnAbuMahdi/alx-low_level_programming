2d15bc649d24dc66dd13caf88a0768fba0a150c4
ibnAbuMahdi <r.muhammad@fubk.edu.ng>
2022-09-08 19:15:35 -0700

    UNIX is basically ..

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - prints to stderr
+ * Description: it prints to standard error stream
+ * Return: it always returns 0
+ */
+int main(void)
+{
+	fprintf(stderr, "and that piece of art is useful\"
+	- Dora Korpar, 2015-10-19\n");
+	return (0);
+}
