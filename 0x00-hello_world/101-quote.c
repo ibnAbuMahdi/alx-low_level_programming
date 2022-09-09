df972977cad9cd79f9a8d4140226099cdd32a0b8
ibnAbuMahdi <r.muhammad@fubk.edu.ng>
2022-09-08 19:32:29 -0700

    UNIX is a simple ..

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -10,7 +10,7 @@
  */
 int main(void)
 {
-	fprintf(stderr, NC "and that piece of art is useful\"
-	- Dora Korpar, 2015-10-19\n");
+	fprintf(stderr, "%s\n", "and that piece of art is useful\"
+	- Dora Korpar, 2015-10-19");
 	return (0);
 }
