e3de9016e59e808e2755565cf3b85a60581ea672
ibnAbuMahdi <r.muhammad@fubk.edu.ng>
2022-09-08 17:37:48 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 
+/**
+ * main - This function prints text using the stdio function puts()
+ * return - returns 0, always
+ */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
-	return(0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
