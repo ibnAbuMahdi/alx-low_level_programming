f7df29d11cf617821f562259168af9c6ffda1545
ibnAbuMahdi <r.muhammad@fubk.edu.ng>
2022-09-10 02:52:51 -0700

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - the entry point
+ * Description: prints the alphabets in lower case
+ * Return: always returns 0
+ */
+int main(void)
+{
+	/*
+	 *@c: is a character variable
+	 */
+	int c = '0';
+
+	while (c < 'f')
+	{
+		putchar(c);
+		c = c + 1;
+		if (c == '9' + 1)
+		{
+			c = 'a';
+		}
+		if (c == 'f')
+		{
+			putchar('\n');
+		}
+	}
+	return (0);
+}
