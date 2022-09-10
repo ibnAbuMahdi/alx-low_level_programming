f34a50fc345e4e8fe886ddee09c963354342a059
ibnAbuMahdi <r.muhammad@fubk.edu.ng>
2022-09-09 21:26:29 -0700

    3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - the entry point
+ * Description: prints the alphabets in lower case
+ * Return: always returns 0
+ */
+int main(void)
+{
+	/*
+	 *@c: is a character variable
+	 */
+	char c = 'a';
+
+	while (c != 'Z' + 1)
+	{
+		putchar(c);
+		c = c + 1;
+		if (c == 'z' + 0)
+		{
+			c = 'A' + 0;
+		}
+		if (c == 'Z' + 1)
+		{
+			putchar('\n' + 0);
+		}
+	}
+	return (0);
+}
