b85bf330833c7235be6523c66d6483f281d6cf1b
ibnAbuMahdi <r.muhammad@fubk.edu.ng>
2022-09-10 05:08:02 -0700

    The success combination ..

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -8,49 +8,39 @@
 int main(void)
 {
 	/*
-	 *@c: is a character variable
+	 * @c: is a character variable
+	 * @b: integer for the second loop
+	 * @a: integer for the third loop
 	 */
 	int c = '0';
 
-	while (c < '9' + 1)
+	for (; c < '9' + 1; c++)
 	{
-		/**
-		 * @b: integer for the second loop
-		 */
 		int b = '0';
 
-		while (b < '9' + 1)
+		for (; b < '9' + 1; b++)
 		{
-			if (c < b)
-			{
-				/**
-				* @a: integer for the third loop
-				*/
-				int a = '0';
+			int a = '0';
 
-				while (a < '9' + 1)
+			for (; a < '9' + 1; a++)
+			{
+				if (c < b &&  b < a)
 				{
-					if (b < a)
+					putchar(c);
+					putchar(b);
+					putchar(a);
+					if (c != '7' || b != '8' || a != '9')
 					{
-						putchar(c);
-						putchar(b);
-						putchar(a);
-						if (c != '7' || b != '8' || a != '9')
-						{
-							putchar(',');
-							putchar(' ');
-						}
-						if (a == '9' && c == '7' && b == '8')
-						{
-							putchar('\n');
-						}
+						putchar(',');
+						putchar(' ');
 					}
-					a = a + 1;
+					if (a == '9' && c == '7' && b == '8')
+					{
+						putchar('\n');
+					}
 				}
 			}
-			b = b + 1;
 		}
-		c = c + 1;
 	}
 	return (0);
 }
