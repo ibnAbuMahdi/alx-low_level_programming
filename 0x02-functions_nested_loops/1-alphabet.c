4c00623ecc07d3f5287767605b5c80bce14d906e
ibnAbuMahdi <r.muhammad@fubk.edu.ng>
2022-09-13 12:09:28 -0700

    sometimes ..

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include <unistd.h>
+#ifndef _putchar
+#include "main.h"
+#endif
+/**
+ * main - program entry point
+ * Return: always 0
+ * */
+
+int main(void)
+{
+	/**
+	 * print_alphabet - prints the alphabets
+	 */
+	print_alphabet();
+	return (0);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,1 +1,11 @@
 int _putchar(char c);
+
+void print_alphabet(void)
+{
+	char c[] = "abcdefghijklmnopqrstuvwxyz\n";
+	int i = 0;
+	for (; i < 27; i++)
+	{
+		_putchar(c[i]);
+	}
+}
