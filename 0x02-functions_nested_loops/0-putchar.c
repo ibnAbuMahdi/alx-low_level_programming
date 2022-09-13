daa15a57fcfb6f90d8477b1047e4cb741548cbbb
ibnAbuMahdi <r.muhammad@fubk.edu.ng>
2022-09-13 10:35:34 -0700

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,24 @@
 #include <unistd.h>
+#include "main.h"
 /**
- * main - the entry point
- * Return: always returns 0
+ * main - program entry point
+ * Return: always 0
+ *
  */
+
 int main(void)
 {
-	write(2,"_putchar\n",9);
+	/**
+	 * _putchar() - function that prints a character at a time
+	 * @c: buffer for the _putchar()
+	 * @i: iteration counter
+	 */
+	char c[] = "_putchar\n";
+	int i = 0;
+
+	for (; i < 9; i++)
+	{
+		_putchar(c[i]);
+	}
 	return (0);
 }
