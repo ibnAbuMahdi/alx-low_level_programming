d4378a1370472bce02954011d3e7a3003ab531d0
ibnAbuMahdi <r.muhammad@fubk.edu.ng>
2022-09-13 16:54:20 -0700

    There are only 3 ..

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,30 @@
+#include <unistd.h>
+#ifndef main_h
+#include "main.h"
+#endif
+
+/**
+ * print_last_digit - prints the last digit of a number
+ * Return: returns the value of the last digit
+ * @n: the integer to compute
+ */
+int print_last_digit(int n)
+{
+	/**
+	 * @r: the remainder to be returned
+	 */
+	if (n > 0)
+	{
+		r = n % 10;
+
+		_putchar(r + '0');
+		return (r);
+	}
+	else
+	{
+		r = (-1 * n) % 10;
+
+		_putchar(r + '0');
+		return (r);
+	}
+}
