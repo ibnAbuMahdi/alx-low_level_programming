7457d8ac1711d4024bccac45ba36940cb639cf7a
ibnAbuMahdi <r.muhammad@fubk.edu.ng>
2022-09-13 19:16:48 -0700

    98 Battery ..

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -18,7 +18,7 @@ void print_to_98(int n)
 			printf("%d, ", n);
 			n--;
 		}
-		printf(n);
+		printf("%d\n", n);
 	}
 	else
 	{
@@ -27,7 +27,7 @@ void print_to_98(int n)
 			printf("%d, ", n);
 			n++;
 		}
-		printf(n);
+		printf("%d\n", n);
 
 	}
 }
