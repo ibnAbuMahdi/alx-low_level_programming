f7f316c3f08a65689453bb843181d2cc6de3e2cc
ibnAbuMahdi <r.muhammad@fubk.edu.ng>
2022-09-14 08:40:14 -0700

    test

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - program entry point
+ * Return: always 0
+ */
+int main(void)
+{
+	/**
+	 * @c: loop counter
+	 * @n1: first int
+	 * @n2: second int
+	 * @sum: sum of n1 and n2
+	 */
+	int c = 2;
+	long int n1 = 1;
+	long int n2 = 2;
+	long int sum;
+
+	while (c < 99)
+	{
+		printf("%ld, ", n1);
+		sum = n2 + n1;
+		n1 = n2;
+		n2 = sum;
+		c++;
+	}
+	printf("%ld\n", n1);
+	return (0);
+}
