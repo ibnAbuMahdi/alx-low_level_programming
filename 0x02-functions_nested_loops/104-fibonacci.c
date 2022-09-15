a8f6b76f81201219979c5b49fd319e825e0ce166
ibnAbuMahdi <r.muhammad@fubk.edu.ng>
2022-09-14 18:21:39 -0700

    test2

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -13,18 +13,18 @@ int main(void)
 	 * @sum: sum of n1 and n2
 	 */
 	int c = 2;
-	long int n1 = 1;
-	long int n2 = 2;
-	long int sum;
+	unsigned long int n1 = 1;
+	unsigned long int n2 = 2;
+	unsigned long int sum;
 
 	while (c < 99)
 	{
-		printf("%ld, ", n1);
+		printf("%lu, ", n1);
 		sum = n2 + n1;
 		n1 = n2;
 		n2 = sum;
 		c++;
 	}
-	printf("%ld\n", n1);
+	printf("%lu\n", n1);
 	return (0);
 }
