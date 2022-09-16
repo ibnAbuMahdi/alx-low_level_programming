80f1d65fe54a2904f47fe4173210bbd05b68848e
ibnAbuMahdi <r.muhammad@fubk.edu.ng>
2022-09-16 16:01:54 -0700

    7

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * print_diagonal - prints diagonal of size n made from
+ * @n: the diagonal size
+ */
+
+void print_diagonal(int n)
+{
+	/**
+	 * @c: the number
+	 * @i: loop counter
+	 */
+	int i;
+
+	if (n > 0)
+	{
+		for (i = 0; i < n; i++)
+		{
+			int c = 0;
+
+			for (; c < i; c++)
+			{
+				if (c == i - 1)
+				{
+					_putchar('\\');
+				}
+				else
+				{
+					_putchar(' ');
+				}
+			}
+			_putchar('\n');
+		}
+	}
+	_putchar('\n');
+}
