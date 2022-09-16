460ebac53327845c92c15194dada9db81ce3c82f
ibnAbuMahdi <r.muhammad@fubk.edu.ng>
2022-09-16 15:26:23 -0700

    5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -19,9 +19,9 @@ void more_numbers(void)
 		{
 			if (c > 9)
 			{
-				_putchar(c / 10);
+				_putchar((c / 10) + '0');
 			}
-			_putchar(c % 10);
+			_putchar((c % 10) + '0');
 		}
 		_putchar('\n');
 	}
