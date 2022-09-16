c018bfa64fdf8368f13d1787ef285263fce50bfc
ibnAbuMahdi <r.muhammad@fubk.edu.ng>
2022-09-16 16:33:40 -0700

    7

---
+++ 0x04-more_functions_nested_loops/7-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+int main(void)
+{
+ print_diagonal(0);
+
+     print_diagonal(2);
+
+         print_diagonal(10);
+
+	     print_diagonal(-4);
+
+	        return (0);
+}
--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -33,5 +33,8 @@ void print_diagonal(int n)
 			_putchar('\n');
 		}
 	}
-	_putchar('\n');
+	else
+	{
+		_putchar('\n');
+	}
 }
