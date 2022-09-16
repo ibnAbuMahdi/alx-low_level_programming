3e599fa6fbc164fb87e17d0de4e0d711add4904a
ibnAbuMahdi <r.muhammad@fubk.edu.ng>
2022-09-16 15:32:22 -0700

    5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -10,11 +10,11 @@ void more_numbers(void)
 	 * @c: the number
 	 * @i: loop counter
 	 */
-	int c = 0;
 	int i;
 
-	for(i = 0; i < 10; i++)
+	for (i = 0; i < 10; i++)
 	{
+		int c = 0;
 		for (; c < 15; c++)
 		{
 			if (c > 9)
