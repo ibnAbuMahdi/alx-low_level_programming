3273e3a7ea361006c05b197ff914a2babc4a2115
ibnAbuMahdi <r.muhammad@fubk.edu.ng>
2022-09-16 17:10:29 -0700

    9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+/**
+ * main - famous FizzBuzz program for 100
+ * Return: always 0
+ */
+
+int main(void)
+{
+	/**
+	 * @c: the number
+	 */
+	int c = 1;
+
+	for (; c < 101; c++)
+	{
+		if (c % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else if (c % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else if (c % 15 == 0)
+		{
+			printf("FizzBuzz");
+		}
+		else
+		{
+			printf("%d", c);
+		}
+		if (c != 100)
+		{
+			printf(" ");
+		}
+	}
+	printf("\n");
+
+	return (0);
+}
