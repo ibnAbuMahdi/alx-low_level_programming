e648d62ab322ba7d9a1b9bee3507de72e3107d7e
ibnAbuMahdi <r.muhammad@fubk.edu.ng>
2022-09-19 21:21:14 -0700

    6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -9,13 +9,35 @@ void puts2(char *s)
 {
 	/**
 	 * @i: loop and string counter
+	 * @j: length holder
 	 */
 	int i = 0;
+	int j = _strlen(s);
 
-	while (*(s + i * 2) > 0)
+	while (*(s + i * 2) > 0 && i * 2 < j)
 	{
 		_putchar(*(s + i * 2));
 		i++;
 	}
 	_putchar('\n');
+}
+
+/**
+ * _strlen - counts
+ * Return: length
+ * @s: the string
+ */
+
+int _strlen(char *s)
+{
+	/**
+	 * @i: counter
+	 */
+	int i = 0;
+
+	while (*(s + i) != 0)
+	{
+		i++;
+	}
+	return (i);
 }
