9c4a01deb9a4a7dc846122f3cd5ceb5fa6a65bab
ibnAbuMahdi <r.muhammad@fubk.edu.ng>
2022-09-21 04:23:12 -0700

    1

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * _strncat - concatenates two strings
+ * @dest: the string to append to
+ * @src: the string to be appended
+ * @n: number of bytes to copy
+ * Return: pointer to a string
+ */
+
+char *_strncat(char *dest, char  *src, int n)
+{
+	/**
+	 * @i: src index tracker
+	 * @st: operation switch
+	 * @j: dest index tracker
+	 */
+	int i = 0;
+	int st = 0;
+	int j = 0;
+
+	while (*(src + i) > 0 && i < n)
+	{
+		if (st > 0)
+		{
+			*(dest + j) = *(src + i);
+			i++;
+			j++;
+		}
+		else if (*(dest + j) > 0)
+		{
+			j++;
+		}
+		else if (*(dest + j) == 0)
+		{
+			st = 1;
+		}
+	}
+	return (dest);
+}
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -4,4 +4,8 @@
 void _putchar(char c);
 
 char *_strcat(char *, char *);
+
+char *_strncat(char *dest, char *src, int n);
+
+
 #endif
