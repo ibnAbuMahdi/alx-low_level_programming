ca878646473a168b39a1aac8b33568bc3d06dc79
ibnAbuMahdi <r.muhammad@fubk.edu.ng>
2022-09-23 14:57:42 -0700

    100

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -11,19 +11,17 @@ char *rot13(char *s)
 	/**
 	 * @i: loop counter
 	 */
-	int i = 0;
+	int i = -1;
 
 	while (*(s + i) != 0)
 	{
+		i++;
 		while ((s[i] > 64 && s[i] < 91) || (s[i] > 96 && s[i] < 123))
 		{
 			if (s[i] < 77 || s[i] < 109)
 				*(s + i) = *(s + i) + 13;
-			else
-				*(s + i) = *(s + i) - 13;
 			i++;
-		}
-		i++;
+		}	
 	}
 	return (s);
 }
