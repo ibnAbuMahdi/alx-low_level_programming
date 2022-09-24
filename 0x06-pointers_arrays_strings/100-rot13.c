b660637a1233e2ae064163fff4fb7fde426bf14f
ibnAbuMahdi <r.muhammad@fubk.edu.ng>
2022-09-23 18:32:56 -0700

    I don see am for this one

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -13,15 +13,16 @@ char *rot13(char *s)
 	 */
 	int i = -1;
 
-	while (*(s + i) != 0)
+	while (i == -1 || *(s + i) != 0)
 	{
 		i++;
 		while ((s[i] > 64 && s[i] < 91) || (s[i] > 96 && s[i] < 123))
 		{
-			if (s[i] < 77 || s[i] < 109)
-				*(s + i) = *(s + i) + 13;
+			if (s[i] < 78 || (s[i] < 110 && s[i] > 96))
+				*(s + i) = s[i] + 26;
+			*(s + i) = s[i] - 13;
 			i++;
-		}	
+		}
 	}
 	return (s);
 }
