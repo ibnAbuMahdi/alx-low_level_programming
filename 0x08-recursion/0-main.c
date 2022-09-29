f7d5284a5395ee9d7d4578f982460032e96a59b5
ibnAbuMahdi <r.muhammad@fubk.edu.ng>
2022-09-28 18:12:18 -0700

    0

---
+++ 0x08-recursion/0-main.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+#include <stdio.h>
+
+
+
+/**
+ *
+ *  * simple_print_buffer - prints buffer in hexa
+ *
+ *   * @buffer: the address of memory to print
+ *
+ *    * @size: the size of the memory to print
+ *
+ *     *
+ *
+ *      * Return: Nothing.
+ *
+ *       */
+
+int main(void)
+{
+	_puts_recursion("Puts with recursion");
+
+	    return (0);
+}
