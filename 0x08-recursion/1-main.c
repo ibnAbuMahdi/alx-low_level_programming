af90f0ca45151a85f18844d570f5d2556aeb41bb
ibnAbuMahdi <r.muhammad@fubk.edu.ng>
2022-09-28 18:30:38 -0700

    1

---
+++ 0x08-recursion/1-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+#include <stdio.h>
+
+
+
+/**
+ *
+ *  * simple_print_buffer - prints buffer in hexa
+ *       */
+
+int main(void)
+{
+ _print_rev_recursion("\nColton Walker");
+	    return (0);
+}
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -4,4 +4,6 @@
 void _putchar(char c);
 
 void _puts_recursion(char *);
+
+void _print_rev_recursion(char *);
 #endif
