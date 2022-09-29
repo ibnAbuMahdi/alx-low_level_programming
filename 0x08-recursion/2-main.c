81e27701583abfe064f4ff0c77931e5b58b9f293
ibnAbuMahdi <r.muhammad@fubk.edu.ng>
2022-09-28 18:53:56 -0700

    2

---
+++ 0x08-recursion/2-main.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+#include <stdio.h>
+
+
+
+/**
+ *
+ *  * simple_print_buffer - prints buffer in hexa
+ *       */
+
+int main(void)
+{
+int n;
+
+
+
+    n = _strlen_recursion("Corbin Coleman");
+
+        printf("%d\n", n);
+
+	    return (0);
+}
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -6,4 +6,8 @@ void _putchar(char c);
 void _puts_recursion(char *);
 
 void _print_rev_recursion(char *);
+
+int _strlen_recursion(char *);
+
+
 #endif
