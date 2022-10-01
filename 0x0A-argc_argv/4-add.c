b80209c32c950b2857e91eda884f22017d6c6f28
ibnAbuMahdi <r.muhammad@fubk.edu.ng>
2022-09-30 20:03:02 -0700

    4

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - add and prints its arguments
+ * @argc: number of arguments
+ * @argv: the arguments
+ * Return: always 0
+ */
+
+int main(int argc, char **argv)
+{
+	char **end;
+	long int sum = 0;
+	long int num;
+	int i;
+	char *p = "";
+
+	end = &p;
+	if (argc < 2)
+		printf("%d\n", 0);
+	for (i = 1; i < argc; i++)
+	{
+		num = strtol(argv[i], end, 10);
+		if (**end == 0)
+		{
+			sum = sum + num;
+		}
+		else
+		{
+			printf("%s\n", "Error");
+			return (1);
+		}
+	}
+	printf("%ld\n", sum);
+	return (0);
+}
