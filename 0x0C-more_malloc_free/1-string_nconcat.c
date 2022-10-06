5050a833f171a96e10e668f9bec2c12a98a97936
ibnAbuMahdi <r.muhammad@fubk.edu.ng>
2022-10-06 14:41:18 -0700

    1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -14,15 +14,15 @@ char *string_nconcat(char *a, char *s, unsigned int n)
 	unsigned int i = 0;
 	unsigned int j = 0;
 
-	f = malloc(_strlen(a) + n);
-	if (!f)
-		return (NULL);
 	if (a == NULL)
 		a = "";
 	if (s == NULL)
 		s = "";
 	if (n > _strlen(s))
 		n = _strlen(s);
+	f = malloc(_strlen(a) + n);
+	if (!f)
+		return (NULL);
 	for (i = 0; i < _strlen(a); i++)
 		f[i] = a[i];
 	for (j = 0; j < n; j++, i++)
