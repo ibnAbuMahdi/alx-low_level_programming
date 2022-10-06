f12668dc23b61876984699c3667f0e9634e0d0c4
ibnAbuMahdi <r.muhammad@fubk.edu.ng>
2022-10-06 14:34:18 -0700

    1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,7 +1,11 @@
 #include "main.h"
 
 /**
- *
+ * string_nconcat - concatenates two strings
+ * @a: string to concatenate full
+ * @s: string to concatenate n chars from
+ * @n: number of chars from @s to concatenate
+ * Return: pointer to the newly allocated space for the concat
  */
 
 char *string_nconcat(char *a, char *s, unsigned int n)
@@ -9,7 +13,7 @@ char *string_nconcat(char *a, char *s, unsigned int n)
 	char *f;
 	unsigned int i = 0;
 	unsigned int j = 0;
-	
+
 	f = malloc(_strlen(a) + n);
 	if (!f)
 		return (NULL);
@@ -21,14 +25,16 @@ char *string_nconcat(char *a, char *s, unsigned int n)
 		n = _strlen(s);
 	for (i = 0; i < _strlen(a); i++)
 		f[i] = a[i];
-	for(j = 0; j < n; j++, i++)
+	for (j = 0; j < n; j++, i++)
 		f[i] = s[j];
 	f[i] = '\0';
 	return (f);
 }
 
 /**
- *
+ * _strlen - get the length of string
+ * @s: the string
+ * Return: length as int
  */
 
 unsigned int _strlen(char *s)
