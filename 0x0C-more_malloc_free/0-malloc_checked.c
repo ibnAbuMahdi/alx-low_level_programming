15d26b65e82a311d271998953ef8e14fcfae2cc5
ibnAbuMahdi <r.muhammad@fubk.edu.ng>
2022-10-06 07:58:40 -0700

    0

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,17 +1,18 @@
 #include "main.h"
 #include <limits.h>
 /**
- *
+ * malloc_checked - allocates memory
+ * @b: memory size
+ * Return: pointer to the memory
  */
 
 void *malloc_checked(unsigned int b)
 {
 	void *s;
-       
+
 	s = malloc(b);
 
 	if (b < INT_MAX && s)
 		return (s);
-	else
-		exit(98);
+	exit(98);
 }
