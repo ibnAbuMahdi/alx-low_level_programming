bdf397f451a85d772944dfc965511ae98210dc1a
ibnAbuMahdi <r.muhammad@fubk.edu.ng>
2022-10-11 10:31:04 -0700

    0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,11 +1,15 @@
 #ifndef DOG_H
 #define DOG_H
-/*declare dog with members name, age and owner
+/**
+ * struct dog - declare dog with members name, age and owner
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
  */
 typedef struct dog
 {
 	char	*name;
-	float 	age;
+	float	age;
 	char	*owner;
 } dog_t;
 
