8b15db428d9a339a82b743d4bab7fe019a04dcd7
ibnAbuMahdi <r.muhammad@fubk.edu.ng>
2022-10-10 21:12:14 -0700

    0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,10 +1,11 @@
 #ifndef DOG_H
 #define DOG_H
-
+/*declare dog with members name, age and owner
+ */
 struct dog {
-	char *name;
-	float age;
-	char *owner;
+	char	*name;
+	float 	age;
+	char	*owner;
 };
 
 #endif /*DOG_H*/
