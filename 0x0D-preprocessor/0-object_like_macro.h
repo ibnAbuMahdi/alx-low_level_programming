92ac26c7ebbbce56812e334daaabcd4f13418931
ibnAbuMahdi <r.muhammad@fubk.edu.ng>
2022-10-10 20:31:01 -0700

    0

---
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -0,0 +1,4 @@
+#ifndef SIZE
+#define SIZE 1024
+
+#endif /*0-OBJECT_LIKE_MICRO_H*/
---
+++ 0x0D-preprocessor/main.h
@@ -0,0 +1,19 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdlib.h>
+void _putchar(char);
+
+void *malloc_checked(unsigned int b);
+
+char *string_nconcat(char *, char *, unsigned int);
+
+unsigned int _strlen(char *);
+
+void *_calloc(unsigned int, unsigned int);
+
+int *array_range(int min, int max);
+
+void *_realloc(void *, unsigned int, unsigned int);
+
+
+#endif 
