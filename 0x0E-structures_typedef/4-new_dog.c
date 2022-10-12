2a1f06d7c7bf44ef59de68047090864d7ddc14ca
ibnAbuMahdi <r.muhammad@fubk.edu.ng>
2022-10-11 23:45:39 -0700

    4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -12,39 +12,35 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *new;
-	char *nname;
-	char *nowner;
 	int i = 0;
 
 	new = malloc(sizeof(struct dog));
-	if (name == NULL)
-		name = "\0";
-	if (owner == NULL)
-		owner = "\0";
-	nname = malloc(sizeof(name) * sizeof(char));
-	nowner = malloc(sizeof(owner) * sizeof(char));
-	if (!new || !nname || !nowner)
+	if (!new)
 	{
 		free(new);
-		free(nname);
-		free(nowner);
+		return (NULL);
+	}
+	new->name = malloc(sizeof(name) * sizeof(char));
+	new->owner = malloc(sizeof(owner) * sizeof(char));
+	if (!new->name || !new->owner)
+	{
+		free(new->name);
+		free(new->owner);
 		return (NULL);
 	}
 	while (name != NULL && name[i])
 	{
-		*(nname + i) = name[i];
+		*(new->name + i) = name[i];
 		i++;
 	}
-	*(nname + i) = '\0';
+	*(new->name + i) = '\0';
 	i = 0;
 	while (name != NULL && owner[i])
 	{
-		*(nowner + i) = owner[i];
+		*(new->owner + i) = owner[i];
 		i++;
 	}
-	*(nowner + i) = '\0';
-	new->name = nname;
-	new->owner = nowner;
+	*(new->owner + i) = '\0';
 	if (age >= 0)
 		new->age = age;
 	else
