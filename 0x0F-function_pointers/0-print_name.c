8614686165a088a321706afce092b4b261b01ba6
ibnAbuMahdi <r.muhammad@fubk.edu.ng>
2022-10-12 09:27:00 -0700

    0

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,4 +1,5 @@
 #include "function_pointers.h"
+#include <stdlib.h>
 
 /**
  * print_name - prints name using function pointer f
@@ -8,5 +9,18 @@
 
 void print_name(char *name, void (*f)(char *))
 {
-	f(name);
+	char *buf;
+	int i, ln;
+
+	for (ln = 0; name[ln]; ln++)
+		;
+	buf = malloc(ln + 1);
+	if (buf)
+	{
+		for (i = 0; i < ln; i++)
+			buf[i] = name[i];
+		buf[i] = '\0';
+
+		f(buf);
+	}
 }
