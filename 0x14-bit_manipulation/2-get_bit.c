ad1ce5fa00914d4404b2aafa93db978cce16df97
ibnAbuMahdi <r.muhammad@fubk.edu.ng>
2022-10-27 18:57:53 -0700

    2

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * get_bit - return bit value at index
+ * @n: int to parse
+ * @index: index of the bit to get
+ * Return: bit value
+ */
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned int bit = 0;
+
+	n &= (1 << index);
+	while (n)
+	{
+		bit = (n & 1) > bit ? 1 : bit;
+		n >>= 1;
+	}
+	return (bit);
+}
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -8,4 +8,6 @@ unsigned int binary_to_uint(const char *);
 unsigned int _pow(unsigned int, unsigned int);
 
 void print_binary(unsigned long int);
+
+int get_bit(unsigned long int, unsigned int);
 #endif /*MAIN_H*/
