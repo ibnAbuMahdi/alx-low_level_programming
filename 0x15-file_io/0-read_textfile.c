344e71d8aea669dfef29d7d50acbf02fccd2de3e
ibnAbuMahdi <r.muhammad@fubk.edu.ng>
2022-10-31 05:10:03 -0700

    0.0

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stddef.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/types.h>
+
+/**
+ * read_textfile - reads c chars from file *f to stdout
+ * @c: number of chars to print
+ * @f: pointer to the file name
+ * Return: number of chars printed or 0 if failed
+ */
+
+ssize_t read_textfile(const char *f, size_t c)
+{
+	char *buf;
+	int fd, readno;
+	size_t	writeno;
+
+	buf = malloc(c);
+	if (!f || !buf)
+		return (0);
+	fd = open(f, O_RDONLY);
+	if (fd < 0)
+		return (0);
+	readno = read(fd, buf, c);
+	if (readno < 0)
+		return (0);
+	writeno = write(STDOUT_FILENO, buf, c);
+	if (writeno < c)
+		return (0);
+	return (writeno);
+}
---
+++ 0x15-file_io/main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stddef.h>
+#include <sys/types.h>
+
+int _putchar(char);
+
+ssize_t read_textfile(const char *, size_t);
+#endif /*MAIN_H*/
