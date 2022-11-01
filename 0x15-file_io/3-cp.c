6d9417357ccbd13476f1866ec64337df7fcbd927
ibnAbuMahdi <r.muhammad@fubk.edu.ng>
2022-11-01 14:42:19 -0700

    3.1

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -15,7 +15,7 @@
 
 int main(int ac, char **av)
 {
-	int fd_from, fd_to, rto;
+	int fd_from, fd_to, rto, mask;
 	struct stat fs;
 
 	if (ac != 3)
@@ -27,14 +27,16 @@ int main(int ac, char **av)
 	fd_to = open(av[2], O_WRONLY | O_TRUNC);
 	if (fd_to < 0)
 		fd_to = creat(av[2], S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
-	if (fd_to < 0 || (rto == 0 && (fs.st_mode & (1 << 7)) != (1 << 7)))
+	mask = (1 << 7) | (1 << 4) | (1 << 1);
+	if (fd_to < 0 || (rto == 0 && (fs.st_mode & mask) > 0))
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", av[2]);
 		exit(99);
 	}
 
 	rto = stat(av[1], &fs);
-	if (rto == 0 && (fs.st_mode & (1 << 8)) != (1 << 8))
+	mask = (1 << 8) | (1 << 5) | (1 << 2);
+	if (rto == 0 && (fs.st_mode & mask) > 0)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
 		call_close(fd_to);
