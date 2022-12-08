3bdb7e841ab179068eb68959b22c51e6afc21f0a
ibnAbuMahdi <r.muhammad@fubk.edu.ng>
2022-12-08 15:06:39 -0800

    1

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,52 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+
+/**
+ * dlistint_len - prints elements of a listint_t list
+ * @h: head node of the list
+ * Return: number of elements in the list
+ */
+
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t i = 0;
+	dlistint_t *temp = (dlistint_t *) h;
+	
+	if (temp){
+		if (temp->next && temp->prev)
+		{
+			while (temp->prev)
+				temp = temp->prev;
+			while (temp)
+			{
+				temp = temp->next;
+				i++;
+			}
+		}
+		else if (temp->next)
+		{
+			while (temp)
+			{
+				temp = temp->next;
+				i++;
+			}
+		}
+		else if (temp->prev)
+		{
+			while (temp)
+			{
+				temp = temp->prev;
+				i++;
+			}
+		}
+		else
+		{
+			temp = temp->next;
+			i++;
+		}
+	}
+	return (i);
+}
+
