a4e05671978a42f6fa8461c93b4313db70fc50d3
ibnAbuMahdi <r.muhammad@fubk.edu.ng>
2022-12-09 06:37:51 -0800

    2

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -3,22 +3,23 @@
 #include <stdlib.h>
 
 /**
- * add_nodeint - adds a node to a linked list
+ * add_dnodeint - adds a node to a linked list
  * @head: pointer to a node pointer
  * @n: the int n member of the node
  * Return: pointer to the added node;
  */
 
-listint_t *add_nodeint(listint_t **head, const int n)
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-	listint_t *temp = NULL;
+	dlistint_t *temp = NULL;
 
 	if (head)
 	{
-		temp = malloc(sizeof(listint_t));
+		temp = malloc(sizeof(dlistint_t));
 		if (!temp)
 			return (NULL);
 		temp->next = *head;
+		temp->prev = NULL;
 		temp->n = n;
 		*head = temp;
 	}
--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -22,7 +22,7 @@ size_t print_dlistint(const dlistint_t *);
 
 size_t dlistint_len(const dlistint_t *);
 
-dlistint_t *add_nodeint(dlistint_t **, const int);
+dlistint_t *add_dnodeint(dlistint_t **, const int);
 
 dlistint_t *add_nodeint_end(dlistint_t **, const int);
 
