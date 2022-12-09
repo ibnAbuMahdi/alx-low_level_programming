cfbfe3e79b89f16a72c500d117f47716794338db
ibnAbuMahdi <r.muhammad@fubk.edu.ng>
2022-12-09 06:09:02 -0800

    1.1

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -16,18 +16,7 @@ size_t print_dlistint(const dlistint_t *h)
 
 	if (temp)
 	{
-		if (temp->next && temp->prev)
-		{
-			while (temp->prev)
-				temp = temp->prev;
-			while (temp)
-			{
-				printf("%d\n", temp->n);
-				temp = temp->next;
-				i++;
-			}
-		}
-		else if (temp->next)
+		if (temp->next)
 		{
 			while (temp)
 			{
