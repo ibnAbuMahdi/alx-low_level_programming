865af458d2d57e779b799ce7246fad55a8f18875
ibnAbuMahdi <r.muhammad@fubk.edu.ng>
2022-12-12 15:29:05 -0800

    0

---
+++ 0x18-dynamic_libraries/main.h
@@ -0,0 +1,43 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+void _putchar(char c);
+
+char *_memset(char *, char, unsigned int);
+
+char *_memcpy(char *, char *, unsigned int);
+
+char *_strchr(char *, char);
+
+unsigned int _strspn(char *, char *);
+
+char *_strstr(char *, char *);
+
+char *_strpbrk(char *s, char *accept);
+
+int _islower(int c);
+
+int _isalpha(int c);
+
+int _abs(int n);
+
+int _isupper(int c);
+
+int _isdigit(int c);
+
+int _strlen(char *s);
+
+void _puts(char *);
+
+char *_strcpy(char *, char *);
+
+int _atoi(char *s);
+
+char *_strcat(char *, char *);
+
+char *_strncat(char *, char *, int);
+
+char *_strncpy(char *, char *, int);
+
+int _strcmp(char *, char *);
+#endif
