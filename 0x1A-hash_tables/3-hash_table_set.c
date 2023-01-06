5e938697fc808401ff961f7640afd2757a1cb91b
ibnAbuMahdi <r.muhammad@fubk.edu.ng>
2023-01-06 09:03:36 -0800

    considered ht->size == 0

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -14,6 +14,8 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	hash_node_t *node;
 	unsigned long int index;
 
+	if (!ht || ht->size == 0)
+		return (0);
 	if (!key || !value || strlen(key) == 0)
 		return (0);
 	node = malloc(sizeof(hash_node_t));
