8adc7ad36a49fb5f73872b507543b229dea482be
ibnAbuMahdi <r.muhammad@fubk.edu.ng>
2023-01-06 03:34:25 -0800

    for 0

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,29 @@
+#include "hash_tables.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * hash_table_create - creates a NULL-filled hash table
+ * @size: table's size
+ * Return: the pointer to the table or NULL if failed
+ */
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *table = malloc(sizeof(hash_table_t));
+	unsigned long int i = 0;
+
+	if (!table)
+		return (NULL);
+
+	table->size = size;
+	table->array = calloc(table->size, sizeof(hash_node_t *));
+	if (!(table->array))
+		return (NULL);
+
+	for (; i < table->size; i++)
+		table->array[i] = NULL;
+
+	return (table);
+}
